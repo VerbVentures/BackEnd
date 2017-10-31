cda38a8c4b590fa632ae11a9176bfb29c022f128
Nathan Glover <nlglover@crimson.ua.edu>
2017-10-30 20:27:52 -0500

    updating models

--- verb_ventures_api/api/models.py
+++ verb_ventures_api/api/models.py
@@ -38,11 +38,15 @@ class Student(models.Model):
     def __str__(self):
         return str(self.user)
 
+
 class Session(models.Model):
     session_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     session_dt = models.DateField()
     admin = models.ForeignKey(Admin, on_delete=models.CASCADE)
     session_students = models.ManyToManyField(Student)
+
+    def __str__(self):
+        return '%s (%s, %s)' % (str(self.session_id), str(self.admin), str(self.session_dt))
 
 
 class Verb(models.Model):
@@ -50,24 +54,37 @@ class Verb(models.Model):
     verb = models.CharField(max_length=20)
     definition = models.TextField()
 
+    def __str__(self):
+        return self.verb
 
 class Animation(models.Model):
     animation_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     verb = models.ForeignKey(Verb, on_delete=models.CASCADE)
     image_address = models.URLField(max_length=2083)
 
+    def __str__(self):
+        return 'Animation for %s: %s' % (self.verb.verb, str(self.animation_id))
+
 
 class VerbPack(models.Model):
     verb_pack_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=260)
     admin = models.ForeignKey(Admin, on_delete=models.CASCADE)
     verb_pack_verbs = models.ManyToManyField(Verb)
     user_verb_packs = models.ManyToManyField(VerbVentureUser)
+
+    def __str__(self):
+        return self.title
 
 
 class LearnedVerb(models.Model):
     learned_verb_id =  models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     verb = models.ForeignKey(Verb, on_delete=models.CASCADE)
-    session = models.ForeignKey(Session, on_delete=models.CASCADE)
+    session = models.ForeignKey(Session, db_index=True, on_delete=models.CASCADE)
+    student = models.ForeignKey(Student, db_index=True, on_delete=models.CASCADE)
     created_dt = models.DateTimeField(auto_now_add=True)
     correct = models.BooleanField()
     tries = models.IntegerField()
+
+    def __str__(self):
+        return str(self.learned_verb_id)
